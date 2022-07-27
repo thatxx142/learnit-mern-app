cc0359e1845860c188c57daa511b60dafe33d115
Thatxx142 <thatxx142@gmail.com>
2022-07-27 18:49:08 +0700

    update 3

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
     process.env.NODE_ENV !== 'production'
-        ? 'https://learnit-mern-app.herokuapp.com'
+        ? 'https://learnit-mern-app.herokuapp.com/api'
         : ''
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
