eda1179327c6e8cf1ecc71d526ff126616948727
Thatxx142 <thatxx142@gmail.com>
2022-07-27 19:29:56 +0700

    update 4

--- server/index.js
+++ server/index.js
@@ -40,6 +40,6 @@ app.use(cors())
 app.use('/api/auth', authRouter)
 app.use('/api/posts', postRouter)
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
